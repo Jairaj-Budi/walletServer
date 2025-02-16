fb7fe4ca9cb459afef2a3e404fda4e2c42cc4e23
murali <muralibadiginti1999@gmail.com>
2025-02-16 23:08:36 +0530

    feat : fixed mongoDB connection issue

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 
 export default () => ({
 
-        MONGODB_URI: process.env.DB || 'mongodb+srv://wallet:walletProject@cluster0.ulxx5.mongodb.net/wallet',
+        MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://wallet:walletProject@cluster0.ulxx5.mongodb.net/wallet',
       
     });
     
